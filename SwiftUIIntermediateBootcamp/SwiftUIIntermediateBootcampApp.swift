686e09c5303456b66cc057648e55651d0cf0f9bc
Rossana Laricchia <rossana.laricchia@gmail.com>
2022-05-25 18:35:30 +0200

    Initial Commit

---
+++ SwiftUIIntermediateBootcamp/SwiftUIIntermediateBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntermediateBootcampApp.swift
+//  SwiftUIIntermediateBootcamp
+//
+//  Created by Rossana Laricchia on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntermediateBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
